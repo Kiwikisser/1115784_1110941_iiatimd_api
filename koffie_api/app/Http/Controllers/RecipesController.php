ee6128f403ee83d98bf34acbfe8884d4bc4bf96f
mauri985 <s1110941@student.hsleiden.nl>
2020-07-08 16:05:16 +0200

    refactored controller to return the right user recipe response data upon creation (generated id)

--- koffie_api/app/Http/Controllers/RecipesController.php
+++ koffie_api/app/Http/Controllers/RecipesController.php
@@ -21,17 +21,25 @@ class RecipesController extends Controller
     public function store(Request $request){
         $data = $request->all();
         $randomId = Str::random(9);
-        
+        $new_recipe_response = $data;
         $new_recipe = new UserRecipe();
         $new_recipe->recipe_id = $randomId;
-        $new_recipe->user_name = "evt auth user/variable"; 
+        $new_recipe->user_name = "mauriccio"; 
         $new_recipe->recipe_name = $data['recipe_name']; 
         $new_recipe->recipe_ingredients = $data['recipe_ingredients']; 
         $new_recipe->coffee_bean = $data['coffee_bean'];
         $new_recipe->coffee_servings = $data['coffee_servings'];
         $new_recipe->coffee_prep_time = $data['coffee_prep_time'];
         $new_recipe->save();
-    
-        return "testresponse";
+        $response_data = [
+            "recipe_id" => $randomId,
+            "user_name" => "mauriccio",
+            "recipe_name" => $data['recipe_name'],
+            "recipe_ingredients" => $data['recipe_ingredients'],
+            "coffee_bean" => $data['coffee_bean'],
+            "coffee_servings" => $data['coffee_servings'],
+            "coffee_prep_time" => $data['coffee_prep_time']
+        ];
+        return response()->json($response_data);
     }
 }
