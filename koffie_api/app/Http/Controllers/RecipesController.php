305fdd33aae8187b55cb2b93c281572adb4cbf0e
mauri985 <s1110941@student.hsleiden.nl>
2020-07-09 13:18:58 +0200

    updated recipe api to receive online(post) & offline(room) responses from app

--- koffie_api/app/Http/Controllers/RecipesController.php
+++ koffie_api/app/Http/Controllers/RecipesController.php
@@ -21,25 +21,36 @@ class RecipesController extends Controller
     public function store(Request $request){
         $data = $request->all();
         $randomId = Str::random(9);
-        $new_recipe_response = $data;
         $new_recipe = new UserRecipe();
-        $new_recipe->recipe_id = $randomId;
-        $new_recipe->user_name = "mauriccio"; 
-        $new_recipe->recipe_name = $data['recipe_name']; 
-        $new_recipe->recipe_ingredients = $data['recipe_ingredients']; 
-        $new_recipe->coffee_bean = $data['coffee_bean'];
-        $new_recipe->coffee_servings = $data['coffee_servings'];
-        $new_recipe->coffee_prep_time = $data['coffee_prep_time'];
-        $new_recipe->save();
-        $response_data = [
-            "recipe_id" => $randomId,
-            "user_name" => "mauriccio",
-            "recipe_name" => $data['recipe_name'],
-            "recipe_ingredients" => $data['recipe_ingredients'],
-            "coffee_bean" => $data['coffee_bean'],
-            "coffee_servings" => $data['coffee_servings'],
-            "coffee_prep_time" => $data['coffee_prep_time']
-        ];
-        return response()->json($response_data);
+        if(count($data) === 7){ // witouth internet connection, data comes from room, no response needed
+            $new_recipe->recipe_id = $data['recipeId'];
+            $new_recipe->user_name = $data['user_name'];
+            $new_recipe->recipe_name = $data['recipe_name']; 
+            $new_recipe->recipe_ingredients = $data['recipe_ingredients']; 
+            $new_recipe->coffee_bean = $data['coffee_bean'];
+            $new_recipe->coffee_servings = $data['coffee_servings'];
+            $new_recipe->coffee_prep_time = $data['coffee_prep_time'];
+            $new_recipe->save();
+        }
+        if(count($data) === 6){  // with internet connection, data comes from form
+            $new_recipe->recipe_id = $randomId;
+            $new_recipe->user_name = $data['user_name'];
+            $new_recipe->recipe_name = $data['recipe_name']; 
+            $new_recipe->recipe_ingredients = $data['recipe_ingredients']; 
+            $new_recipe->coffee_bean = $data['coffee_bean'];
+            $new_recipe->coffee_servings = $data['coffee_servings'];
+            $new_recipe->coffee_prep_time = $data['coffee_prep_time'];
+            $new_recipe->save();
+            $response_data = [
+                "recipe_id" => $randomId,
+                "user_name" => $data['user_name'],
+                "recipe_name" => $data['recipe_name'],
+                "recipe_ingredients" => $data['recipe_ingredients'],
+                "coffee_bean" => $data['coffee_bean'],
+                "coffee_servings" => $data['coffee_servings'],
+                "coffee_prep_time" => $data['coffee_prep_time']
+            ];
+            return response()->json($response_data);
+        }
     }
 }
