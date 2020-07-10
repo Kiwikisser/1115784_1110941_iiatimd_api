f0dc87682ecb301a1a87f58ee4182734fb511761
Amos Peperkamp <amos.peperkamp@gmail.com>
2020-07-10 04:38:41 +0200

    laid base for coffee tables with standard coffee

---
+++ koffie_api/database/seeds/CoffeeSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CoffeeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+      // DB::table('time')->insert([
+      //   'recipe_name' => ,
+      //   'record' => ,
+      //   'time_number' => ,
+      //   'weekday' => ,
+      // ]);
+    }
+}
